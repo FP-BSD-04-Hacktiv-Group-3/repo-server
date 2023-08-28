abdf46f12e07dc5095e404637d571c2bc9c872f0
andreaszzz18 <andreaszzz18@gmail.com>
2023-08-28 09:02:50 +0700

    feat: cart category history image store wishlist xendit

--- services/app/routers/router.js
+++ services/app/routers/router.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const productRouter = require("./productRouter");
+const categoryRouter = require("./categoryRouter")
 
-router.use("/products", productRouter)
+router.use("/products", productRouter);
+router.use("/categories", categoryRouter);
 
 module.exports = router
