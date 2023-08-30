ab503a7be477b1f3301d0bfd2d24fd1b990bd1e2
GunardiCahyadi <cahyadigunardi@gmail.com>
2023-08-30 13:38:24 +0700

    fix: merge conflict

--- services/app/app.js
+++ services/app/app.js
@@ -3,25 +3,12 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 4002;
 const cors = require("cors");
-<<<<<<< HEAD
-const router = require("./router/index");
+const { router } = require("./router");
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(router);
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-=======
-const {router} = require('./router')
-
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(router)
->>>>>>> 81e6ba802430c78e6583de3d72706240ff98cf0c
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
