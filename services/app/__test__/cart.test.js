6d07e1357058483c1ab2fc4c00a610a3774be78a
GunardiCahyadi <cahyadigunardi@gmail.com>
2023-08-31 12:38:08 +0700

    feat: test cart

---
+++ services/app/__test__/cart.test.js
@@ -0,0 +1,120 @@
+const request = require("supertest");
+const app = require("../app");
+const { Product, Store, Cart, Image } = require("../models");
+
+describe("Carts Endpoints", function () {
+  beforeAll(async () => {
+    const productData = require("../db/products.json");
+    const storeData = require("../db/stores.json");
+    const cartData = require("../db/carts.json");
+    const imageData = require("../db/images.json");
+
+    await Store.bulkCreate(storeData);
+    await Product.bulkCreate(productData);
+    await Cart.bulkCreate(cartData);
+    await Image.bulkCreate(imageData);
+  });
+
+  afterAll(async () => {
+    await Product.destroy({
+      truncate: true,
+      cascade: true,
+      restartIdentity: true,
+    });
+
+    await Store.destroy({
+      truncate: true,
+      cascade: true,
+      restartIdentity: true,
+    });
+  });
+
+  //   it("Fetch Products without any filters", async function () {
+  //     const response = await request(app).get("/product");
+
+  //     expect(response.status).toEqual(200);
+  //     expect(response.body.length).toEqual(12);
+  //   });
+
+  it("Fetch Cart by userid", async function () {
+    const response = await request(app).get("/carts/64ed5f7942e9c21d8fe5e699");
+
+    console.log(response.body);
+    expect(response.status).toEqual(200);
+    expect(response.body.data).toEqual(expect.any(Object));
+    expect(response.body.data.length).toEqual(3);
+  });
+
+  it("Create cart", async function () {
+    const response = await request(app).post("/carts/addCart").send({
+      UserId: "64ed5f7942e9c21d8fe5e699",
+      quantity: "5",
+      ProductId: "1",
+      totalPrice: "100000",
+    });
+
+    expect(response.status).toEqual(201);
+    expect(response.body).toHaveProperty(
+      "message",
+      "Successfully create a Cart"
+    );
+  });
+
+  //   it("Fetch Products with with product name search", async function () {
+  //     const response = await request(app).get(
+  //       "/product?productName=Piring hitam kayu"
+  //     );
+
+  //     expect(response.status).toEqual(200);
+  //     expect(response.body[0]).toEqual(expect.any(Object));
+  //     expect(response.body[0].name).toEqual("Piring hitam kayu");
+  //   });
+
+  //   it("Fetch Products including other table/model", async function () {
+  //     const response = await request(app).get("/product");
+
+  //     expect(response.status).toEqual(200);
+  //     expect(response.body[0]).toHaveProperty("Images");
+  //   });
+
+  //   it("Fetch Product Detail", async function () {
+  //     const response = await request(app).get("/product/1");
+
+  //     expect(response.status).toEqual(200);
+  //     expect(response.body).toEqual(expect.any(Object));
+  //   });
+
+  //   // buat post product agak bingung karena ada hubungannya ama file/image
+  //   // it("")
+
+  it("Delete Cart", async function () {
+    const id = 12;
+    const response = await request(app).delete("/carts/12");
+
+    expect(response.status).toEqual(200);
+    expect(response.body).toEqual(expect.any(Object));
+    expect(response.body.message).toEqual("Successfully delete a Cart " + id);
+  });
+  it("Delete Cart by userid", async function () {
+    const userId = 12;
+    const response = await request(app).delete("/carts/deleteByUserId/12");
+
+    expect(response.status).toEqual(200);
+    expect(response.body).toEqual(expect.any(Object));
+    expect(response.body.message).toEqual(
+      "Successfully delete Carts by userId: " + userId
+    );
+  });
+
+  it("Edit cart", async function () {
+    const id = 1;
+    const response = await request(app).put("/carts/editCart/1").send({
+      UserId: "64ed5f7942e9c21d8fe5e694",
+      quantity: "3",
+      ProductId: "3",
+    });
+    console.log(response.body, "0000");
+    expect(response.status).toEqual(200);
+    expect(response.body.message).toEqual("Successfully update a Cart " + id);
+  });
+});
